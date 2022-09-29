d8af3cecdd6f306c81bef4b406276ba1976cbfd3
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2022-09-29 10:42:50 -0300

    Finalização da questão 3

--- src/Questao3.java
+++ src/Questao3.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class Questao3 {
     public static void main(String[] args) {
+        //Declaração da função scanner
         Scanner scanner = new Scanner(System.in);
+
+        //Enunciado da questão
+
         System.out.println("QUESTÃo 3");
         System.out.println(" Faça um algoritmo com as seguintes opções: Vote na mais simpática:");
         System.out.println("1- Docinho");
@@ -10,15 +14,21 @@ public class Questao3 {
         System.out.println("3- Florzinha");
         System.out.println("0- Sair");
         System.out.println("Ao final, apresente a quantidade de votos de cada uma delas e mais simpática.");
+
+        //declaração de variáveis
         int totalvotos = 0;
         int votosinvalidos = 0;
         int voto = 1;
         int docinho = 0;
         int lindinha = 0;
         int florzinha = 0;
+
+        //Estrutura de repetição dos votos
         while (voto != 0){
             System.out.println("Digite seu voto: ");
             voto = scanner.nextInt();
+
+            //Estruturação dos casos
             switch (voto){
                 case 1:
                     docinho += 1;
@@ -36,9 +46,21 @@ public class Questao3 {
             }
             totalvotos +=1;
         }
+
+        //Saída: Resultado da votação
         System.out.println("O total de votos foi de "+totalvotos+" votos, onde "+votosinvalidos+" foram inválidos e:");
         System.out.println("Docinho recebeu "+docinho+" votos");
         System.out.println("Lindinha recebeu "+lindinha+" votos");
         System.out.println("Florzinha recebeu "+florzinha+" votos");
+
+        //Condicional para declarar a venccedora
+        if (docinho > lindinha && docinho > florzinha) {
+            System.out.println("Docinho ganhou com "+docinho+" votos.");
+        } else if (lindinha > docinho && lindinha > florzinha) {
+            System.out.println("Lindinha ganhou com "+lindinha+" votos.");
+        } else if (florzinha > lindinha && florzinha > docinho) {
+            System.out.println("Florzinha ganhou com "+florzinha+" votos.");
+
+        }
     }
 }
