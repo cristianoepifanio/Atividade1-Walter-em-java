b74533586eace0045367da955d8582f9ec495e11
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2022-09-29 11:31:21 -0300

    renomeando a class para "Questao1"

--- src/Questao1.java
+++ src/Questao1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Questao1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("QUESTÃO 1");
